3bc1f479e2fff3c09d9b6b5098f01b65c54ec7ec
Eugene Pak <eugene@koyoshiga.local>
2025-12-10 00:30:12 -0600

    Thread pool implementation

--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -1,20 +1,53 @@
 #include "threadpool.h"
+#include <iostream>
 
-using namespace std;
-
-ThreadPool::ThreadPool(size_t num_threads, size_t queue_size)
-    : task_queue_(queue_size), shutdown_(false) {
+// Constructor: launches a given number of worker threads
+ThreadPool::ThreadPool(size_t num_threads) : stop(false) {
+    for (size_t i = 0; i < num_threads; ++i) {
+        workers.emplace_back(&ThreadPool::worker_thread, this);
+    }
 }
 
+// Destructor: joins all threads
 ThreadPool::~ThreadPool() {
+    {
+        // Lock the mutex to safely change the 'stop' flag
+        unique_lock<mutex> lock(queue_mutex);
+        stop = true;
+    }
+
+    // Wake up all threads so they can see the 'stop' flag
+    condition.notify_all();
+
+    // Wait for all threads to finish their execution
+    for (thread &worker : workers) {
+        worker.join();
+    }
 }
 
-void ThreadPool::worker_loop() {
-}
+// The function that each worker thread runs in a loop
+void ThreadPool::worker_thread() {
+    while (true) {
+        function<void()> task;
 
-bool ThreadPool::submit(function<void()> task) {
-    return false;
-}
+        {
+            // Lock the mutex and wait for a task or a stop signal
+            unique_lock<mutex> lock(queue_mutex);
+            
+            condition.wait(lock, [this] {
+                return stop || !tasks.empty();
+            });
 
-void ThreadPool::shutdown() {
+            // If the pool is stopped and there are no more tasks, exit the thread
+            if (stop && tasks.empty()) {
+                return;
+            }
+
+            // Get the next task from the queue
+            task = move(tasks.front());
+            tasks.pop();
+        }
+
+        task();
+    }
 }
